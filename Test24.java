798dc938d696cd84b918a655db4e4ffca39bdc51
ofb777 <ofb241223@gmail.com>
2025-11-27 12:38:52 +0800

    提交24题

--- Test24.java
+++ Test24.java
@@ -1,3 +1,22 @@
-public class Test24 {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Test24 {
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+
+        try {
+            FileInputStream input = new FileInputStream("config.properties");
+            prop.load(input);
+            input.close();
+
+            System.out.println("数据库主机: " + prop.getProperty("database.host"));
+            System.out.println("数据库端口: " + prop.getProperty("database.port"));
+            System.out.println("数据库用户名: " + prop.getProperty("database.username"));
+
+        } catch (IOException e) {
+            System.out.println("读取配置文件失败: " + e.getMessage());
+        }
+    }
 }
